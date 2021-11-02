8a2d9a50bfd08fa0c2b07ced4b39837b19fcfe55
Ian Williams <iancrpb@hotmail.com>
2021-11-02 11:25:01 -0600

    Proyecto en General

---
+++ src/java/com/ulatina/controller/CarouselView.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ulatina.controller;
+
+import com.ulatina.service.Product;
+import com.ulatina.service.ProductService;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import org.primefaces.model.ResponsiveOption;
+
+/**
+ *
+ * @author Ian
+ */
+@ManagedBean (name = "carouselView")
+@ViewScoped
+public class CarouselView implements Serializable {
+
+    private List<Product> products;
+
+    private List<ResponsiveOption> responsiveOptions;
+
+    @ManagedProperty("#{productService}")
+    private ProductService service;
+
+    @PostConstruct
+    public void init() {
+        products = service.getProducts(9);
+        responsiveOptions = new ArrayList<>();
+        responsiveOptions.add(new ResponsiveOption("1024px", 3, 3));
+        responsiveOptions.add(new ResponsiveOption("768px", 2, 2));
+        responsiveOptions.add(new ResponsiveOption("560px", 1, 1));
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setService(ProductService service) {
+        this.service = service;
+    }
+
+    public List<ResponsiveOption> getResponsiveOptions() {
+        return responsiveOptions;
+    }
+
+    public void setResponsiveOptions(List<ResponsiveOption> responsiveOptions) {
+        this.responsiveOptions = responsiveOptions;
+    }
+}
